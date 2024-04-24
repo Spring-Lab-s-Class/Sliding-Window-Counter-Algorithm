e3d8920bcc680404ed52ae254ecb7515a426d156
jay-so <merry9504@naver.com>
2024-04-24 23:56:27 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/systemdesign/slidingwindowcounter/SlidingwindowcounterApplication.java
@@ -0,0 +1,13 @@
+package com.systemdesign.slidingwindowcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlidingwindowcounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlidingwindowcounterApplication.class, args);
+    }
+
+}
